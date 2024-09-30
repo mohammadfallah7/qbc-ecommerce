2d73f41dcb5d963c170d4fcfc5d401400dc0039e
zinbyousefi <yousefizeinb@gmail.com>
2024-09-30 20:21:36 +0330

    feat: :sparkles: create comment with api

--- src/components/ProductComments.tsx
+++ src/components/ProductComments.tsx
@@ -9,19 +9,21 @@ interface IProductCommentsProps {
 const ProductComments: React.FC<IProductCommentsProps> = ({ id }) => {
   const { data: product } = useSingleProduct(id!);
 
-  return (
+  return product?.reviews.length === 0 ? (
+    <p className="text-sm">هیچ نظری برای این محصول وجود ندارد</p>
+  ) : (
     <div>
-      {product?.reviews.map((comment) => (
+      {product?.reviews.map((review) => (
         <div
           className="bg-base-200 rounded-md my-5 p-4 flex justify-between gap-4"
-          key={comment._id}
+          key={review._id}
         >
           <div className="flex flex-col gap-3">
-            <span className="text-sm">{comment.user}</span>
-            <p>{comment.name}</p>
-            <ProductStars rate={comment.rating} />
+            <span className="text-sm">{review.name}</span>
+            <p>{review.comment}</p>
+            <ProductStars rate={review.rating} />
           </div>
-          <span className="text-sm">{getDate(comment.createdAt)}</span>
+          <span className="text-sm">{getDate(review.createdAt)}</span>
         </div>
       ))}
     </div>
